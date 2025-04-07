18ab8ac1a4f1e2a3a7d0c8a86b165f6207c45261
siniseong <shinhuiseong07@gmail.com>
2025-04-07 19:40:22 +0900

    feat :: favicon 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   title: "딸깍",
   description: "부산소프트웨어마이스터고 학생들을 위한 코드 공유 서비스입니다.",
   generator: "siniseong",
+  icons: {
+    icon: "/site-logo.svg",
+  },
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
