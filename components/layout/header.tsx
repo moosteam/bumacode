f39ebaeb3c9755be80b9174ec9654f52012357a1
siniseong <shinhuiseong07@gmail.com>
2025-04-07 20:17:25 +0900

    faet :: IP 로딩 스크롤 추가

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,10 +3,12 @@
 import Link from "next/link"
 import Image from "next/image"
 import { useEffect, useState } from "react"
+import { Loader2 } from "lucide-react"
 
 export default function Header() {
   const [ip, setIp] = useState("");
   const [isIpVisible, setIsIpVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   
   useEffect(() => {
     const fetchIp = async () => {
@@ -16,6 +18,8 @@ export default function Header() {
         setIp(data.ip);
       } catch (error) {
         console.error("IP 주소를 가져오는 데 실패했습니다.", error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -77,7 +81,15 @@ export default function Header() {
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5">
                 <path fillRule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clipRule="evenodd" />
               </svg>
-              <span>내 IP · {isIpVisible ? ip : formattedIp}</span>
+              <span className="inline-flex items-center gap-2">
+                {isLoading ? (
+                  <>
+                    내 IP · <Loader2 className="w-4 h-4 animate-spin" />
+                  </>
+                ) : (
+                  `내 IP · ${isIpVisible ? ip : formattedIp}`
+                )}
+              </span>
             </div>
           </div>
         </div>
