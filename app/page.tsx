dd27b28317d1476ecf1d2fe1a60de3ef00f32c5b
siniseong <shinhuiseong07@gmail.com>
2025-04-08 00:05:01 +0900

    chore :: 오른쪽 하단에 있던 코드 작성 경로 이동 버튼 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -148,15 +148,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      <div className="fixed bottom-8 right-8">
-        <Link
-          href="/write"
-          className="bg-blue-500 hover:bg-blue-700 text-white p-4 rounded-full shadow-lg flex items-center justify-center"
-        >
-          <Code size={24} />
-        </Link>
-      </div>
       <Footer />
     </main>
   )
